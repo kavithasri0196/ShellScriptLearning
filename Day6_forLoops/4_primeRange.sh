23873932c4bbf7cac4672e00b8595ae2130ecd62
ParthibanRajalingam <kavithasri0196@gmail.com>
2022-06-30 11:24:25 +0530

    day6 forloop assignment completed

---
+++ Day6_forLoops/4_primeRange.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+function isPrime(){
+input=$1;
+m=$(($input /2 ));
+
+if [ $input -eq 0 ] || [ $input -eq 1 ]
+then
+echo false;
+return;
+
+else
+for (( i=2;i<$m;i++ ))
+do
+if [ $(($input % $i)) -eq 0 ]
+then
+echo false;
+return;
+fi
+done
+fi
+echo true;
+}
+
+read -p "Enter upper bound of prime number:" x
+read -p "Enter lower bound of prime number:" y
+echo Printing prime numbers between $x and $y
+for(( i=$x; i<=$y;i++ ));
+do
+result=$(isPrime $i)
+if [ "$result" = "true" ];
+	then
+	echo $i
+fi
+done
